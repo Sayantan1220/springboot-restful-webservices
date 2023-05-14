d0fb036cf9cf23eabcd8fe03d109e4f072ad9197
Sayantan <sayantan.das208@gmail.com>
2023-05-15 00:24:04 +0530

    Dockering Webservices

--- src/main/java/com/microservices/springboot/service/ScoutService.java
+++ src/main/java/com/microservices/springboot/service/ScoutService.java
@@ -1,10 +1,8 @@
 package com.microservices.springboot.service;
 
 import com.microservices.springboot.dto.ScoutDto;
-import com.microservices.springboot.entity.Scout;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ScoutService {
     ScoutDto createScout(ScoutDto scoutDto);
